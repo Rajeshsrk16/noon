ffff329c5963850ed1ce22c6eb74b43fa099439c
Rajeshsrk16 <rajeshkumar.cse@ksrce.ac.in>
2016-08-30 14:23:33 +0530

    Create Stringreapeted.java

---
+++ Stringreapeted.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Stringrepeated {
+public static void main(String[] args)
+{ 
+	String s="r";
+	String s1="t";
+	
+  Scanner scr=new Scanner(System.in);
+  int n=scr.nextInt();
+ int n1=scr.nextInt();
+  for(int i=0;i<n;i++)
+
+  {
+	 System.out.println(s);
+	  
+  }
+  for(int j=0;j<n1;j++)
+  {
+	  System.out.println(s1);
+  }
+  scr.close();}
+}
